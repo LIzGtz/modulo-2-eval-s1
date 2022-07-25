6b2ed8d4c8149342976d16693b7a3c373492aada
tuNombre <lizethd.gutierrez11@gmail.com>
2022-07-25 00:34:54 -0500

    Aplicacion inicial

---
+++ src/components/QuoteBox.jsx
@@ -0,0 +1,32 @@
+import React from "react"
+import quotes from "./quotes.json"
+import { useState } from "react"
+import './QuoteBox.css'
+
+function QuoteBox() {    
+    const getRandomIndex = (max) => {
+        return Math.floor(Math.random() * (max - 1));
+    }
+    const colors = ['black', 'red', 'blue', 'green', 'purple'];
+
+    const [quoteIndex, setQuoteIndex] = useState(getRandomIndex(quotes.length));
+    const [colorIndex, setColorIndex] = useState(getRandomIndex(colors.length));
+
+    const nextQuote = () => {
+        let newQuoteIndex = getRandomIndex(quotes.length);
+        let newColorIndex = getRandomIndex(colors.length);
+
+        setQuoteIndex(newQuoteIndex);
+        setColorIndex(newColorIndex);
+
+        return newQuoteIndex;
+    }
+
+    return <div className="quote-container">
+        <p style={{color: colors[colorIndex]}}>"{quotes[quoteIndex].quote}"</p>
+        <p style={{color: colors[colorIndex]}}>- {quotes[quoteIndex].author}</p>
+        <button onClick={nextQuote}>Next</button>
+    </div>
+}
+
+export default QuoteBox;
